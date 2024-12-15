3acd3d2fac7d4048dc58459ca6434b9cd6909ffc
张珂玮（Kewei Zhang） <jszkw2005@sjtu.edu.cn>
2024-12-15 15:36:28 +0800

    555

--- src/Regex_Creator_For_User.c
+++ src/Regex_Creator_For_User.c
@@ -8,31 +8,42 @@ struct frontend_regexp **user_fr = NULL;
 
 void initialize_user_types()
 {
-    number_of_user = 3; // 由用户填写
+    number_of_user = 1;
     // 分配struct type 的内存
-    user_types = (struct type *)malloc(number_of_user * sizeof(struct type));
+    user_types = (struct type *)malloc(sizeof(struct type));
 
     user_types[0].name = (char *)malloc(50 * sizeof(char));
-    strcpy(user_types[0].name, "1"); // 设置第一个字符串值
-
-    user_types[1].name = (char *)malloc(50 * sizeof(char));
-    strcpy(user_types[1].name, "2"); // 设置第二个字符串值
-
-    user_types[2].name = (char *)malloc(50 * sizeof(char));
-    strcpy(user_types[2].name, "3"); // 设置第三个字符串值
+    strcpy(user_types[0].name, "If"); // 设置第一个字符串值
 
     user_types[0].id = 1;
-    user_types[1].id = 2;
-    user_types[2].id = 3;
 }
 
 void allocate_and_initialize_user_fr()
 {
-    user_fr = (struct frontend_regexp **)malloc(number_of_user * sizeof(struct frontend_regexp *));
+    user_fr = (struct frontend_regexp **)malloc(1 * sizeof(struct frontend_regexp *));
+    struct char_set whitespace = {" \t", 2}; // \s: space and tab
+    struct char_set non_rparen = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*-_=+[]{}|;:',.<>?/`~ ()", 95};
+    struct char_set no_rparen = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*-_=+[]{}|;:',.<>?/`~ ()", 95};
 
-    user_fr[0] = TFr_String("it's");
-    user_fr[1] = TFr_String("a");
-    user_fr[2] = TFr_String("example");
+    struct frontend_regexp *if_keyword = TFr_String("if");
+    struct frontend_regexp *optional_whitespace = TFr_Star(TFr_CharSet(&whitespace));
+    struct frontend_regexp *left_paren = TFr_SingleChar('(');
+    struct frontend_regexp *condition = TFr_Star(TFr_CharSet(&no_rparen));
+    struct frontend_regexp *right_paren = TFr_SingleChar(')');
+    struct frontend_regexp *left_brace = TFr_SingleChar('{');
+    struct frontend_regexp *body = TFr_Star(TFr_CharSet(&non_rparen));
+    struct frontend_regexp *right_brace = TFr_SingleChar('}');
+
+    user_fr[0] =
+        TFr_Concat(if_keyword,
+                   TFr_Concat(optional_whitespace,
+                              TFr_Concat(left_paren,
+                                         TFr_Concat(condition,
+                                                    TFr_Concat(right_paren,
+                                                               TFr_Concat(optional_whitespace,
+                                                                          TFr_Concat(left_brace,
+                                                                                     TFr_Concat(body,
+                                                                                                right_brace))))))));
 
     return;
 } // if
--- src/main.c
+++ src/main.c
@@ -8,9 +8,11 @@
 #include "../test/test.h"
 #include "../include/Regex_Creator_For_User.h"
 // 读取输入
-void read_inputs(char *file_name, char ***inputs, int *number) {
+void read_inputs(char *file_name, char ***inputs, int *number)
+{
     FILE *fp = fopen(file_name, "r");
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         perror("Failed to open file");
         *number = 0;
         *inputs = NULL;
@@ -19,7 +21,8 @@ void read_inputs(char *file_name, char ***inputs, int *number) {
     size_t capacity = 10;
     size_t count = 0;
     char **array = malloc(capacity * sizeof(char *));
-    if (array == NULL) {
+    if (array == NULL)
+    {
         perror("Failed to allocate memory");
         fclose(fp);
         *number = 0;
@@ -27,22 +30,28 @@ void read_inputs(char *file_name, char ***inputs, int *number) {
         return;
     }
     char buffer[1024];
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
         size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 2] == '\r'&&buffer[len - 1] == '\n') {
+        if (len > 0 && buffer[len - 2] == '\r' && buffer[len - 1] == '\n')
+        {
             buffer[len - 2] = '\0';
             len--;
         }
-        else if (len > 0 && buffer[len - 1] == '\n') {
+        else if (len > 0 && buffer[len - 1] == '\n')
+        {
             buffer[len - 1] = '\0';
             len--;
         }
-        if (count >= capacity) {
+        if (count >= capacity)
+        {
             capacity *= 2;
             char **new_array = realloc(array, capacity * sizeof(char *));
-            if (new_array == NULL) {
+            if (new_array == NULL)
+            {
                 perror("Failed to reallocate memory");
-                for (size_t i = 0; i < count; i++) {
+                for (size_t i = 0; i < count; i++)
+                {
                     free(array[i]);
                 }
                 free(array);
@@ -54,9 +63,11 @@ void read_inputs(char *file_name, char ***inputs, int *number) {
             array = new_array;
         }
         array[count] = malloc((len + 1) * sizeof(char));
-        if (array[count] == NULL) {
+        if (array[count] == NULL)
+        {
             perror("Failed to allocate memory for line");
-            for (size_t i = 0; i < count; i++) {
+            for (size_t i = 0; i < count; i++)
+            {
                 free(array[i]);
             }
             free(array);
@@ -70,58 +81,71 @@ void read_inputs(char *file_name, char ***inputs, int *number) {
     }
     fclose(fp);
     *inputs = array;
-    *number = (int) count;
+    *number = (int)count;
 }
 
-
-
-int main(int argc, char *argv[]) {
-    int test_case =atoi(argv[1]);
+int main(int argc, char *argv[])
+{
+    int test_case = atoi(argv[1]);
     char **inputs;
     int number;
-    if(argc<=1||argc>3)
+    if (argc <= 1 || argc > 3)
     {
         // 参数个数不对
         printf("Too few or too many arguments!\n");
         return 0;
     }
-    if(test_case > 0 && test_case < 11){
+    if (test_case > 0 && test_case < 11)
+    {
         initialize_test_types(test_case);
         allocate_and_initialize_test_fr(test_case);
-        char * file_name = malloc(sizeof(char)*50);
-        strcpy(file_name,"../src/read_files/read_file");
-        strcat(file_name,argv[1]);
+        char *file_name = malloc(sizeof(char) * 50);
+        strcpy(file_name, "../src/read_files/read_file");
+        strcat(file_name, argv[1]);
         read_inputs(file_name, &inputs, &number);
         free(file_name);
     }
-    else if(argc!=3)
+    else if (argc != 3)
     {
         // 参数个数不对
         printf("Too few or too many arguments!\n");
         return 0;
     }
-    else{
-        if(test_case == -1){
+    else
+    {
+        if (test_case == -1)
+        {
             initialize_user_types();
             allocate_and_initialize_user_fr();
             read_inputs(argv[2], &inputs, &number);
         }
-        else{
+        else
+        {
             // 测试用例编号不合法
             printf("Invalid test case number!\n");
             return 0;
         }
     }
-    struct frontend_regexp **fr_regexps ;
+    struct frontend_regexp **fr_regexps;
     struct type *types;
-    int types_num=1;
-    fr_regexps=test_fr;
-    types=test_types;
-    types_num=number_of_test;
+    int types_num = 1;
+    if (test_case == -1)
+    {
+        fr_regexps = user_fr;
+        types = user_types;
+        types_num = number_of_user;
+    }
+    else
+    {
+        fr_regexps = test_fr;
+        types = test_types;
+        types_num = number_of_test;
+    }
     struct simpl_regexp **sr_regexps = (struct simpl_regexp **)malloc(types_num * sizeof(struct simpl_regexp *));
-    printf("types_num:%d\n",types_num);
-    //简化正则表达式
-    for (int i = 0; i < types_num; i++) {
+    printf("types_num:%d\n", types_num);
+    // 简化正则表达式
+    for (int i = 0; i < types_num; i++)
+    {
         print_frontend_regexp(fr_regexps[i]);
         sr_regexps[i] = transform_to_simplified(fr_regexps[i]);
         print_simpl_regexp(sr_regexps[i]);
@@ -131,43 +155,48 @@ int main(int argc, char *argv[]) {
     int *start = (int *)malloc(types_num * sizeof(int));
     int *end = (int *)malloc(types_num * sizeof(int));
     // 生成NFA
-    for (int i = 0; i < types_num; i++) {
-        nfa[i] = build_nfa(sr_regexps[i],start+i,end+i);
+    for (int i = 0; i < types_num; i++)
+    {
+        nfa[i] = build_nfa(sr_regexps[i], start + i, end + i);
     }
     printf("nfa started\n");
-    for(int i = 0;i<types_num;i++)
+    for (int i = 0; i < types_num; i++)
     {
-        printf("start:%d,end:%d\n",start[i],end[i]);
+        printf("start:%d,end:%d\n", start[i], end[i]);
         print(nfa[i]);
     }
     int dst_number = 0;
-    int **dst = (int **)malloc(8*types_num * sizeof(int *));
-    for (int i = 0; i < types_num*8; i++) {
+    int **dst = (int **)malloc(8 * types_num * sizeof(int *));
+    for (int i = 0; i < types_num * 8; i++)
+    {
         dst[i] = (int *)malloc(1024 * sizeof(int));
     }
     int *count = (int *)malloc(1024 * sizeof(int));
-    int *num=malloc(sizeof(int)*1024);
+    int *num = malloc(sizeof(int) * 1024);
     printf("nfa finished\n");
     // 生成DFA
-    struct finite_automata *dfa = NFA2DFA(types_num, nfa, end, &dst_number, dst,count,num);
+    struct finite_automata *dfa = NFA2DFA(types_num, nfa, end, &dst_number, dst, count, num);
     print(dfa);
     printf("dfa finished\n");
-    printf("Line Number %d\n",number);
+    printf("Line Number %d\n", number);
     // 词法分析
-    for (int i=0;i<number;i++){
-    struct tokens tokens=tokenize(dfa, dst, types, dst_number, inputs[i],count,num);
-    printf("Tokens:\n");
-    print_token(tokens);
-    free_tokens(&tokens);
+    for (int i = 0; i < number; i++)
+    {
+        struct tokens tokens = tokenize(dfa, dst, types, dst_number, inputs[i], count, num);
+        printf("Tokens:\n");
+        print_token(tokens);
+        free_tokens(&tokens);
     }
     printf("tokens finished\n");
-    for (int i = 0; i < types_num; i++) {
+    for (int i = 0; i < types_num; i++)
+    {
         free_simpl_regexp_rec(sr_regexps[i]);
         free_frontend_regexp_rec(fr_regexps[i]);
         free_finite_automata(nfa[i]);
     }
 
-    for (int i = 0; i < number; i++) {
+    for (int i = 0; i < number; i++)
+    {
         free(inputs[i]);
     }
     free(inputs);
@@ -179,7 +208,7 @@ int main(int argc, char *argv[]) {
     free(num);
     free(sr_regexps);
     free(nfa);
-    free_types(test_types,types_num);
+    free_types((test_case == -1) ? user_types : test_types, types_num);
     free_for_dfa(dfa);
     return 0;
 }
